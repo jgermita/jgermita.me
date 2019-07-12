245c5b11d6b4f8ec9cbcfe4e0c4252103976181b
Jeremy Paul Germita <jeremy.germita@gmail.com>
2019-07-12 05:04:26 -0700

    robot query works

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"strings"
+
 	"github.com/revel/revel"
 )
 
@@ -19,12 +21,43 @@ func (c App) Me() revel.Result {
 func (c App) Level5() revel.Result {
 	return c.Render()
 }
+
 func (c App) Projects() revel.Result {
 	return c.Render()
 }
+
 func (c App) First() revel.Result {
 	return c.Render()
 }
+
 func (c App) Software() revel.Result {
 	return c.Render()
+}
+
+func (c App) Blog() revel.Result {
+	return c.Render()
+}
+
+var allRobots [5]string
+
+func (c App) Robot() revel.Result {
+	allRobots := [5]string{"sandstorm", "sideslip", "faultline", "echo", "blackout"}
+
+	robot := strings.ToLower(c.Params.Route.Get("robot"))
+	var valid = false
+
+	for i := 0; i < 5; i++ {
+		if robot == allRobots[i] {
+			valid = true
+		}
+
+	}
+
+	if !valid {
+		robot = "Invalid Robot " + robot + "!"
+	}
+
+	robot = strings.Title(robot)
+
+	return c.Render(robot)
 }
